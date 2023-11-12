28de4f2c544479748fb377df94f92b4870e9384e
RandaAsim <randasanfora2015.com>
2023-11-12 02:48:29 -0800

    header commit

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+void display_prompt();
+int execute_command(char *command);
+
+#endif /* SHELL_H */
