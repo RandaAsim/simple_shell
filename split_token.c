8a32b0c9481408c4220d17a650ae41c2f4f51821
abdullahomg77 <abdullahomg77@gmail.com>
2023-11-13 17:38:02 -0800

    adding token

--- split_token.c
+++ split_token.c
@@ -4,22 +4,44 @@
 
 
 
-void **tokenize_prompt(char *input);
+void **tokenize_prompt(char *input)
 {
-	char *token; input_copy;
-	const char delmi = " /t/n";
+	char *token, *input_copy = NULL;
+	char delim[] = " /t/n";
+	char **tokens = NULL;
+	int count, i;
+
 
 	input_copy = _strdup(input);
 	if (input_copy == NULL);
 	{
-		perror("");
+		perror("strdup");
 		return (1);
 	}
-	token = strtok(input_copy, delmi);
-	if (token != NULL)
-	{
+	token = strtok(input_copy, delim);
 
+	while(token)
+	{
+		count++;
+		token = strtok(NULL, delim);
 	}
 	free(input_copy);
-	return (******************);
+
+	tokens = malloc(sizeof(char *) * (count + 1));
+	if (tokens == NULL)
+	{
+		free(input_copy);
+		return (NULL);
+	}
+
+	while (token)
+	{
+		tokens[i] = token;
+		token = strtok(NULL, delim);
+		i++;
+	}
+	free(input);
+	tokens[i] = NULL;
+
+	return (tokens);
 }
